bad6ac362ad17d0482bebae2ea8c838c8c211e08
APMorozov <morozov-sasha200@yandex.ru>
2025-03-29 19:20:18 +0400

    fix vigenere.py and requirements.txt

--- lab1-var11/task1/vigenere.py
+++ lab1-var11/task1/vigenere.py
@@ -6,26 +6,27 @@ def vigenere(plain_text: str, key: str, alphabet: str) -> str:
     :param alphabet: open alphabet
     :return: encrypted text
     """
-    if plain_text == "":
-        return plain_text
-    if alphabet == "":
-        raise Exception("ERROR!Alphabet can`t be empty")
-    if key == "":
-        raise Exception("ERROR!Key can`t be empty")
+    match plain_text, key, alphabet:
+        case '', key, alphabet:
+            return plain_text
+        case plain_text, '' | '':
+            print('ERROR!Key or alphabet can`t be empty')
+        case _:
+            plain_text_tolower = plain_text.lower()
+            key_tolower = key.lower()
+            alphabet_dict = {symbol: index for index, symbol in enumerate(alphabet)}
+            encrypted_answer = ''
 
-    plain_text_tolower = plain_text.lower()
-    key_tolower = key.lower()
-    alphabet_dict = {symbol: index for index, symbol in enumerate(alphabet)}
-    encrypted_answer = ''
-
-    for i in range(len(plain_text_tolower)):
-        current_key = i % len(key_tolower)
-        if abs(alphabet_dict.get(plain_text_tolower[i]) + alphabet_dict.get(key_tolower[current_key])) >= len(alphabet):
-            decode_symbol = alphabet_dict.get(plain_text_tolower[i]) + alphabet_dict.get(key_tolower[current_key]) - len(alphabet)
-            encrypted_answer += alphabet[decode_symbol]
-        else:
-            encrypted_answer += alphabet[alphabet_dict.get(plain_text_tolower[i]) + alphabet_dict.get(key_tolower[current_key])]
-    return encrypted_answer
+            for i in range(len(plain_text_tolower)):
+                current_key = i % len(key_tolower)
+                sum_of_idx = alphabet_dict.get(plain_text_tolower[i]) + alphabet_dict.get(key_tolower[current_key])
+                match sum_of_idx:
+                    case sum_of_idx if sum_of_idx >= len(alphabet):
+                        decode_symbol = sum_of_idx - len(alphabet)
+                        encrypted_answer += alphabet[decode_symbol]
+                    case sum_of_idx:
+                        encrypted_answer += alphabet[sum_of_idx]
+            return encrypted_answer
 
 
 def decode_vigenere(encrypted_text: str, key: str, alphabet: str) -> str:
@@ -36,20 +37,23 @@ def decode_vigenere(encrypted_text: str, key: str, alphabet: str) -> str:
         :param alphabet: open alphabet
         :return: decoded text
         """
-    if encrypted_text == "":
-        return encrypted_text
-    if alphabet == "":
-        raise Exception("ERROR!Alphabet can`t be empty")
-    if key == "":
-        raise Exception("ERROR!Key can`t be empty")
-
-    alphabet_dict = {symbol: index for index, symbol in enumerate(alphabet)}
-    decoded_answer = ''
-    for i in range(len(encrypted_text)):
-        current_key = i % len(key)
-        if (alphabet_dict.get(encrypted_text[i]) - alphabet_dict.get(key[current_key])) < 0:
-            decode_symbol = alphabet_dict.get(encrypted_text[i]) - alphabet_dict.get(key[current_key]) + len(alphabet)
-            decoded_answer += alphabet[decode_symbol]
-        else:
-            decoded_answer += alphabet[alphabet_dict.get(encrypted_text[i]) - alphabet_dict.get(key[current_key])]
-    return decoded_answer
+    match encrypted_text, key, alphabet:
+        case '', key, alphabet:
+            return encrypted_text
+        case encrypted_text, '' | '':
+            print('ERROR!Key or alphabet can`t be empty')
+        case _:
+            alphabet_dict = {symbol: index for index, symbol in enumerate(alphabet)}
+            decoded_answer = ''
+            encrypted_text_tolower = encrypted_text.lower()
+            key_tolower = key.lower()
+            for i in range(len(encrypted_text)):
+                current_key = i % len(key)
+                diff_of_idx = alphabet_dict.get(encrypted_text_tolower[i]) - alphabet_dict.get(key_tolower[current_key])
+                match diff_of_idx:
+                    case diff_of_idx if diff_of_idx < 0:
+                        decode_symbol = diff_of_idx + len(alphabet)
+                        decoded_answer += alphabet[decode_symbol]
+                    case diff_of_idx:
+                        decoded_answer += alphabet[diff_of_idx]
+            return decoded_answer
