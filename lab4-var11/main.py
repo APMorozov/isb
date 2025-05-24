182de8c9df37069fa450a9fd3b5858580ef4cacc
APMorozov <morozov-sasha200@yandex.ru>
2025-05-24 14:34:58 +0400

    make class FindCollision and some methods

--- lab4-var11/main.py
+++ lab4-var11/main.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from task.find_collision import FindCollision
+
+
+generator = FindCollision.generate_number("523444", "2335")
+
+if FindCollision.check_number("e4d09db1a7cb6b495dfdb572c2e4a669f71efd04ca9650a788974cb66698d23392928fdc5bb76ad9e1d36ecdcddc7ad53ff5c626077bd522ffac314698499335", "250034"):
+    print("Yes")
+else:
+    print("No")
+
